d37f233e627aef364cc692e6d0f4abbe9d1f8da5
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-19 00:07:20 +0200

    Connection to the Database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 dotenv.config();
 
@@ -17,16 +18,32 @@ if (process.env.NODE_ENV === 'local') {
     }));
 }
 
+const dbConnect = async () => {
+    try {
+        if (process.env.NODE_ENV !== 'local') {
+            await mongoose.connect(process.env.LOCAL_DB_URI);
+            console.log('Local Database Is Connected');
+        } else {
+            await mongoose.connect(process.env.LOCAL_DB_URI);
+            console.log('Production Database Is Connected');
+        }
+    } catch (error) {
+        console.log('Database connection Failed');
+    };
+}
+
 const PORT = process.env.PORT || 5501;
+
+dbConnect();
 
 console.log('process.env.PORT', process.env.PORT);
 
-// app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
 // Import Database
-mongoose.connect(process.env.MONGO_DB_URI)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Database connected to ${PORT}`);
-        })
-    })
-    .catch((err) => console.log(err));
+// mongoose.connect(process.env.MONGO_DB_URI)
+//     .then(() => {
+//         app.listen(PORT, () => {
+//             console.log(`Database connected to ${PORT}`);
+//         })
+//     })
+//     .catch((err) => console.log(err));
